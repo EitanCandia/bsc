3f057dbb085fa5ca610af71ab95b13bbe6dacbfa
Mati-Maulen <matias.maulen@usm.cl>
2023-07-04 18:02:20 -0400

    subida bsc_proveedor xd

--- BFF-BSC/bsc_proveedor/bsc_proveedor_BFF/src/main/java/com/example/demo/service/proveedorServiceBff1Impl.java
+++ BFF-BSC/bsc_proveedor/bsc_proveedor_BFF/src/main/java/com/example/demo/service/proveedorServiceBff1Impl.java
@@ -6,7 +6,7 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import com.example.demo.restclients.proveedorMicro1;
+import com.example.demo.restclients.proveedormicro2;
 import com.example.demo.dto.proveedorDTO;
 
 @Service
@@ -14,35 +14,35 @@ public class proveedorServiceBff1Impl implements proveedorServiceBff1 {
 
 
     @Autowired
-    proveedorMicro1 micro1;
+    proveedormicro1 micro2;
 
     @Override
     public List<proveedorDTO> proveedorFindAll() {
         // TODO Auto-generated method stub
-        return micro1.findAll();
+        return micro2.findAll();
     }
 
     @Override
     public Optional<proveedorDTO> proveedorFindById(int id) {
         // TODO Auto-generated method stub
-        return micro1.findById(id);
+        return micro2.findById(id);
     }
 
     @Override
     public proveedorDTO proveedorSave(proveedorDTO proveedor) {
         // TODO Auto-generated method stub
-        return micro1.proveedorSave(proveedor);
+        return micro2.proveedorSave(proveedor);
     }
 
     @Override
     public void proveedordelete(int id) {
-        micro1.proveedordelete(id);
+        micro2.proveedordelete(id);
     }
 
     @Override
     public proveedorDTO proveedorUpdate(int id, proveedorDTO proveedor) {
         // TODO Auto-generated method stub
-        return micro1.proveedorUpdate(id, proveedor);
+        return micro2.proveedorUpdate(id, proveedor);
     }
 
 }
