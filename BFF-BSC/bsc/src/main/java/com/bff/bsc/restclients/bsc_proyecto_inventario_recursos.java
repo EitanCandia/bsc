eec1b72a9ec76c007899d0188d85348de96d9284
Mati-Maulen <matias.maulen@usm.cl>
2023-07-04 17:02:21 -0400

    fixing seba ....

--- BFF-BSC/bsc/src/main/java/com/bff/bsc/restclients/bsc_proyecto_inventario_recursos.java
+++ BFF-BSC/bsc/src/main/java/com/bff/bsc/restclients/bsc_proyecto_inventario_recursos.java
@@ -4,8 +4,12 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 
 import com.bff.bsc.dto.Proyecto_inventario_recursosDTO;
 
@@ -22,7 +26,7 @@ public interface bsc_proyecto_inventario_recursos {
     public void deleteById(@PathVariable("id") int id);
 
     @PostMapping(value = "v1/org/bsc_proyecto_inventario_recursos/REST", consumes = "application/json", produces = "application/json")
-    public Proyecto_inventario_recursosDTO save(@RequestBody Proyecto_inventario_recursosDTO bsc_proyecto_inventario_recursos
+    public Proyecto_inventario_recursosDTO save(@RequestBody Proyecto_inventario_recursosDTO bsc_proyecto_inventario_recursos);
     
     @PutMapping(value = "v1/org/bsc_proyecto_inventario_recursos/REST/{id}", consumes = "application/json", produces = "application/json")
     public Proyecto_inventario_recursosDTO update(@PathVariable("id") int id, @RequestBody Proyecto_inventario_recursosDTO bsc_proyecto_inventario_recursos);
