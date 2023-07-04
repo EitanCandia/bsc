9ceca98281aacf23ba6169b5a186ac4bcba2b405
Mati-Maulen <matias.maulen@usm.cl>
2023-07-04 17:02:50 -0400

    fixin dto

--- BFF-BSC/bsc/src/main/java/com/bff/bsc/dto/Proyecto_inventario_recursosDTO.java
+++ BFF-BSC/bsc/src/main/java/com/bff/bsc/dto/Proyecto_inventario_recursosDTO.java
@@ -6,7 +6,7 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
-import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
 
 
 @Data
